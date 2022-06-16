f74e32fadbc8b88f1462ac3e0c0eabf8b12ef5b1
vanna7366 <vanna7366c@co-well.com>
2022-06-16 21:47:49 +0700

    update

--- src/main/java/Utility/BaseUtility.java
+++ src/main/java/Utility/BaseUtility.java
@@ -5,6 +5,7 @@ import org.openqa.selenium.remote.RemoteWebDriver;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeClass;
