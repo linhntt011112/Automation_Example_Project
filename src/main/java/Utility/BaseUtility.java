0b89690c594dd20b865d124e102fb0169164095c
vanna7366 <vanna7366c@co-well.com>
2022-06-16 16:37:14 +0700

    udpate

--- src/main/java/Utility/BaseUtility.java
+++ src/main/java/Utility/BaseUtility.java
@@ -1,7 +1,6 @@
 package Utility;
 
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.ChromeOptions;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.firefox.FirefoxDriver;
