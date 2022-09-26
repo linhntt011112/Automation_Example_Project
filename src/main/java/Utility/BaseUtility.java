81d5512b932d7e7b966fc4be8347f29af2d4581c
linhntt011112 <linhntt011199@gmail.com>
2022-09-26 17:15:08 +0700

    update

--- src/main/java/Utility/BaseUtility.java
+++ src/main/java/Utility/BaseUtility.java
@@ -1,8 +1,5 @@
 package Utility;
 
-import org.openqa.selenium.Capabilities;
-import org.openqa.selenium.remote.RemoteWebDriver;
-
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
@@ -11,10 +8,13 @@ import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Parameters;
 
+import java.util.Arrays;
+import java.util.List;
 import java.util.concurrent.TimeUnit;
 
 public class BaseUtility {
     private static WebDriver driver;
+    List<String> URLitems;
 
     public static WebDriver getDriver() {
         return driver;
@@ -48,39 +48,43 @@ public class BaseUtility {
 
     private static WebDriver initChromeDriver(String appURL, String driverPath) {
         System.out.println("Launching Chrome browser...");
-        System.setProperty("webdriver.chrome.driver", driverPath + "chromedriver-105.exe");
-        String a = System.getProperty("webdriver.chrome.driver");
-        System.out.println(a);
-        ChromeOptions options = new ChromeOptions();
-        options.addArguments("--headless");
-        options.addArguments("--no-sandbox");
-        options.addArguments("--disable-dev-shm-usage");
-
-        driver = new ChromeDriver(options);
-        Capabilities cap = ((RemoteWebDriver) driver).getCapabilities();
-
-        String browserName = cap.getBrowserName();
-        System.out.println(browserName);
-        String os = cap.getPlatform().toString();
+        String os = System.getProperty("os.name");
         System.out.println(os);
-        String v = cap.getVersion().toString();
-        System.out.println(v);
-
+        if (os.contains("Windows")) {
+            System.setProperty("webdriver.chrome.driver", driverPath + "chromedriver.exe");
+            System.out.println("appURL: " + appURL);
+            driver = new ChromeDriver();
+        }
+        else {
+            System.setProperty("webdriver.chrome.driver", driverPath + "chromedriver");
+            ChromeOptions options = new ChromeOptions();   //chrome binary location
+            options.addArguments("--headless");
+            options.addArguments("--no-sandbox");
+            options.addArguments("--disable-dev-shm-usage");
+            driver = new ChromeDriver(options);
+        }
+        System.out.println("appURL: " + appURL);
         driver.manage().window().maximize();
-        System.out.println(appURL);
+
         driver.navigate().to(appURL);
         driver.manage().timeouts().pageLoadTimeout(30, TimeUnit.SECONDS);
         driver.manage().timeouts().implicitlyWait(20, TimeUnit.SECONDS);
         return driver;
     }
 
+    public List<String> getAllURLs() {
+        return URLitems;
+    }
+
     // Run initializeTestBaseSetup function first when this class is called
-    @Parameters({ "browserType", "appURL", "driverPath" })
+    @Parameters({ "browserType", "URLs", "driverPath" })
     @BeforeClass
-    public void initializeTestBaseSetup(String browserType, String appURL, String driverPath) {
+    public void initializeTestBaseSetup(String browserType, String URLs, String driverPath) {
+        URLitems = Arrays.asList(URLs.split("\\s*,\\s*"));
         try {
             // Khởi tạo driver và browser
-            setDriver(browserType, appURL, driverPath);
+//            System.out.println(URLitems.get(0));
+            setDriver(browserType, URLitems.get(0), driverPath);
         } catch (Exception e) {
             System.out.println("Error..." + e.getStackTrace());
         }
--- src/main/java/script/testcases/OpenGoogleTest.java
+++ src/main/java/script/testcases/OpenGoogleTest.java
@@ -8,16 +8,19 @@ import org.testng.annotations.Test;
 import script.pages.OpenGoogleFactory;
 import Utility.*;
 
+import java.util.List;
+
 public class OpenGoogleTest extends BaseUtility{
     private WebDriver driver;
+    private List<WebDriver> allDrivers;
     private OpenGoogleFactory openGoogleFactory;
     private JSONUtility jsonUtility;
-    private CaptureUtility captureUtility;
 
     @BeforeClass
     public void setUp() {
         driver = getDriver();
         jsonUtility = new JSONUtility();
+        System.out.println(getAllURLs());
     }
 
     @Test(priority = 1)
